d6361619a1ff865609e3b462d7582b022ecbb412
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-11-26 00:12:40 +0000

    Added MediaDiscoverer, help to get video/audio inputs

---
+++ Helpers/mediadiscoverer.cpp
@@ -0,0 +1,80 @@
+#include "mediadiscoverer.h"
+
+#include <initguid.h>
+#include <windows.h>
+#include <dshow.h>
+
+HRESULT EnumerateDevices(REFGUID category, IEnumMoniker **ppEnum)
+{
+    // Create the System Device Enumerator.
+    ICreateDevEnum *pDevEnum;
+    HRESULT hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL,
+                                  CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pDevEnum));
+
+    if (SUCCEEDED(hr))
+    {
+        // Create an enumerator for the category.
+        hr = pDevEnum->CreateClassEnumerator(category, ppEnum, 0);
+        if (hr == S_FALSE)
+        {
+            hr = VFW_E_NOT_FOUND;  // The category is empty. Treat as an error.
+        }
+        pDevEnum->Release();
+    }
+    return hr;
+}
+
+void DisplayDeviceInformation(IEnumMoniker *pEnum, QStringList& o_list)
+{
+    IMoniker *pMoniker = NULL;
+    while (pEnum->Next(1, &pMoniker, NULL) == S_OK)
+    {
+        IPropertyBag *pPropBag;
+        HRESULT hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+        if (FAILED(hr))
+        {
+            pMoniker->Release();
+            continue;
+        }
+
+        VARIANT var;
+        VariantInit(&var);
+
+        // Get description or friendly name.
+        hr = pPropBag->Read(L"Description", &var, 0);
+        if (FAILED(hr))
+        {
+            hr = pPropBag->Read(L"FriendlyName", &var, 0);
+        }
+        if (SUCCEEDED(hr))
+        {
+            o_list.push_back(QString::fromStdWString(var.bstrVal));
+            VariantClear(&var);
+        }
+
+        pPropBag->Release();
+        pMoniker->Release();
+    }
+}
+
+void MediaDiscoverer::run()
+{
+    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (SUCCEEDED(hr))
+    {
+        IEnumMoniker *pEnum;
+
+        QStringList list;
+        hr = EnumerateDevices(m_isAudio ? CLSID_AudioInputDeviceCategory : CLSID_VideoInputDeviceCategory, &pEnum);
+        if (SUCCEEDED(hr))
+        {
+            DisplayDeviceInformation(pEnum, list);
+            pEnum->Release();
+        }
+
+        CoUninitialize();
+
+        std::sort(list.begin(), list.end());
+        emit SignalResult(list);
+    }
+}
---
+++ Helpers/mediadiscoverer.h
@@ -0,0 +1,28 @@
+#ifndef MEDIADISCOVERER_H
+#define MEDIADISCOVERER_H
+
+#include <QThread>
+
+class MediaDiscoverer : public QThread
+{
+    Q_OBJECT
+public:
+    explicit MediaDiscoverer(
+        bool isAudio,
+        QObject *parent = nullptr
+    )
+        : QThread(parent)
+        , m_isAudio(isAudio)
+    { }
+
+protected:
+    void run() override;
+
+signals:
+    void SignalResult(QStringList const& list);
+
+private:
+    bool m_isAudio = false;
+};
+
+#endif // MEDIADISCOVERER_H
