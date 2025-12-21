4659a107fe9e77a7c46bdf847c96a3a72c533dca
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-12-21 12:10:16 +0000

    Added AudioConversionUtils

---
+++ Helpers/audioconversionutils.cpp
@@ -0,0 +1,207 @@
+#include "audioconversionutils.h"
+
+AudioConversionUtils& AudioConversionUtils::instance()
+{
+    static AudioConversionUtils utils;
+    return utils;
+}
+
+AudioConversionUtils::AudioConversionUtils()
+{
+    m_hanningFunction.resize(FFT_SAMPLE_COUNT);
+    for (int i = 0; i < FFT_SAMPLE_COUNT / 2; i++)
+    {
+        m_hanningFunction[i] = 0.5f - 0.5f * std::cos((2.0f * float(M_PI) * i) / (FFT_SAMPLE_COUNT - 1));
+        m_hanningFunction[FFT_SAMPLE_COUNT - 1 - i] = m_hanningFunction[i];
+    }
+
+    m_spikeConvFunction.resize(18);
+    for (int i = 0; i < 9; i++)
+    {
+        m_spikeConvFunction[i] = -4.0f + 8.f * i / 8.0f;
+        m_spikeConvFunction[17 - i];
+    }
+}
+
+void AudioConversionUtils::debugAudioFormat(const QAudioFormat &audioFormat)
+{
+    qDebug() << "Debug Audio Format:";
+
+    QString str = "Sample type: ";
+    switch(audioFormat.sampleFormat())
+    {
+    case QAudioFormat::SampleFormat::Int16: str += "Int16"; break;
+    case QAudioFormat::SampleFormat::Int32: str += "Int32"; break;
+    case QAudioFormat::SampleFormat::UInt8: str += "UInt8"; break;
+    case QAudioFormat::SampleFormat::Float: str += "Float"; break;
+    default: str += "Unknown"; break;
+    }
+
+    qDebug() << "Bytes per Sample:" << audioFormat.bytesPerFrame() / audioFormat.channelCount();
+    qDebug() << "Channel Count:" << audioFormat.channelCount();
+    qDebug() << "Sample Rate:" << audioFormat.sampleRate();
+}
+
+const QVector<float> &AudioConversionUtils::getHanningFunction()
+{
+    return instance().m_hanningFunction;
+}
+
+const QVector<float> &AudioConversionUtils::getSpikeConvFunction()
+{
+    return instance().m_spikeConvFunction;
+}
+
+//-----------------------------------------
+// Normalize (type to float)
+//-----------------------------------------
+bool AudioConversionUtils::convertSamplesToFloat(const QAudioFormat &format, const char *data, size_t dataSize, QVector<float> &out)
+{
+    switch(format.sampleFormat())
+    {
+    case QAudioFormat::SampleFormat::Int16:
+    {
+        normalizeType<int16_t>(format, data, dataSize, out);
+        return true;
+    }
+    case QAudioFormat::SampleFormat::Int32:
+    {
+        normalizeType<int32_t>(format, data, dataSize, out);
+        return true;
+    }
+    case QAudioFormat::SampleFormat::UInt8:
+    {
+        normalizeType<uint8_t>(format, data, dataSize, out);
+        return true;
+    }
+    case QAudioFormat::SampleFormat::Float:
+    {
+        out.resize(static_cast<int>(dataSize / sizeof(float)));
+        memcpy(out.data(), data, dataSize);
+        return true;
+    }
+    default: return false;
+    }
+}
+
+template<typename Type>
+void AudioConversionUtils::normalizeType(const QAudioFormat &format, const char *data, size_t dataSize, QVector<float> &out)
+{
+    // Allocate memory here
+    out.resize(static_cast<int>(dataSize / sizeof(Type)));
+    normalizeAudio<Type>(reinterpret_cast<const Type*>(data), out.data(), dataSize / sizeof(Type), true);
+}
+
+template<typename Type>
+void AudioConversionUtils::normalizeAudio(const Type *in, float *out, size_t outSize, bool isLittleEndian)
+{
+    const float rcp = (std::is_unsigned<Type>::value ? 2.0f : 1.0f) / (float)std::numeric_limits<Type>::max();
+    const float sub = std::is_unsigned<Type>::value ? 1.0f : 0.0f;
+    for (size_t c = 0; c < outSize; c++)
+    {
+        out[c] = ((float)(isLittleEndian ? in[c] : byteSwap(in[c])) * rcp - sub);
+    }
+}
+
+//-----------------------------------------
+// Fast Fourier Transform
+//-----------------------------------------
+void AudioConversionUtils::fft(int sampleSize, fftwf_complex *in, fftwf_complex *out)
+{
+    // create a DFT plan
+    fftwf_plan plan = fftwf_plan_dft_1d(sampleSize, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    // execute the plan
+    fftwf_execute(plan);
+
+    // do some cleaning
+    fftwf_destroy_plan(plan);
+    fftwf_cleanup();
+}
+
+void AudioConversionUtils::ifft(int sampleSize, fftwf_complex *in, fftwf_complex *out)
+{
+    // create an IDFT plan
+    fftwf_plan plan = fftwf_plan_dft_1d(sampleSize, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    // execute the plan
+    fftwf_execute(plan);
+
+    // do some cleaning
+    fftwf_destroy_plan(plan);
+    fftwf_cleanup();
+
+    // scale the output to obtain the exact inverse
+    for (int i = 0; i < sampleSize; ++i)
+    {
+        out[i][REAL] /= sampleSize;
+        out[i][IMAG] /= sampleSize;
+    }
+}
+
+void AudioConversionUtils::debugComplex(fftwf_complex *c, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (c[i][IMAG] < 0)
+        {
+            qDebug() << QString::number(c[i][REAL]) + " - " + QString::number(qAbs(c[i][IMAG])) + "i";
+        }
+        else
+        {
+            qDebug() << QString::number(c[i][REAL]) + " + " + QString::number(c[i][IMAG]) + "i";
+        }
+    }
+}
+
+void AudioConversionUtils::fftOutToSpectrogram(int sampleSize, const fftwf_complex *in, QVector<float> &out)
+{
+    if (out.size() != sampleSize / 2)
+    {
+        out.resize(sampleSize / 2);
+    }
+
+    for (int i = 0; i < sampleSize / 2; i++)
+    {
+        float const& real = in[i][REAL];
+        float const& imag = in[i][IMAG];
+        float const mag = std::sqrt(real * real + imag * imag) / (sampleSize / 2);
+
+        // Get magnitude in log scale, this will be [0,-inf]
+        out[i] = 0.0f;
+        if (mag > 0.0f)
+        {
+            constexpr float minMag = -10.0f;
+            constexpr float maxMag = -3.0f;
+            float const logMag = std::log(mag);
+            if (logMag > minMag)
+            {
+                out[i] = 1.0f - ((maxMag - logMag) / (maxMag - minMag));
+            }
+        }
+    }
+}
+
+void AudioConversionUtils::spikeConvolution(int indexStart, int indexEnd, const QVector<float> &in, QVector<float> &out, float threshold)
+{
+    QVector<float> const& spikeConvFunc = instance().getSpikeConvFunction();
+    int outSize = (indexEnd - indexStart) - spikeConvFunc.size() + 1;
+    if (out.size() != outSize)
+    {
+        out.resize(outSize);
+    }
+
+    for(int i = 0; i < outSize; i++)
+    {
+        out[i] = 0.0f;
+        for(int j = 0; j < spikeConvFunc.size(); j++)
+        {
+            out[i] += in[indexStart + i + j] * spikeConvFunc[j];
+        }
+
+        if (out[i] <= threshold)
+        {
+            out[i] = 0.0f;
+        }
+    }
+}
---
+++ Helpers/audioconversionutils.h
@@ -0,0 +1,61 @@
+#ifndef AUDIOCONVERSIONUTILS_H
+#define AUDIOCONVERSIONUTILS_H
+
+#include <QAudioFormat>
+#include <QDebug>
+#include <QMetaEnum>
+#include <QWidget>
+#include <QtMath>
+
+#include <fftw3.h>
+#define REAL 0
+#define IMAG 1
+
+#define FFT_SAMPLE_COUNT    4096
+#define FFT_WINDOW_STEP     1024
+
+typedef QMap<int, float> SpikeIDScore;
+
+class AudioConversionUtils
+{
+private:
+    static AudioConversionUtils& instance();
+    AudioConversionUtils();
+
+public:
+    // Utils
+    static void debugAudioFormat(QAudioFormat const& audioFormat);
+    static QVector<float> const& getHanningFunction();
+    static QVector<float> const& getSpikeConvFunction();
+
+    // Main conversion function
+    static bool convertSamplesToFloat(const QAudioFormat& format, const char* data, size_t dataSize, QVector<float>& out);
+
+    // Fast Fourier Transform
+    static void fft(int sampleSize, fftwf_complex *in, fftwf_complex *out);
+    static void ifft(int sampleSize, fftwf_complex *in, fftwf_complex *out);
+    static void debugComplex(fftwf_complex *c, int size);
+    static void fftOutToSpectrogram(int sampleSize, fftwf_complex const* in, QVector<float>& out);
+    static void spikeConvolution(int indexStart, int indexEnd, QVector<float> const& in, QVector<float>& out, float threshold = 1.0f);
+
+private:
+    // Byte swap template functions
+    inline static uint8_t byteSwap(uint8_t x) { return x; }
+    inline static int8_t byteSwap(int8_t x) { return x; }
+    inline static uint16_t byteSwap(uint16_t x) { return (uint32_t)_byteswap_ulong(x) >> 16; }
+    inline static int16_t byteSwap(int16_t x) { return (int16_t)((uint32_t)_byteswap_ulong(x) >> 16); }
+    inline static uint32_t byteSwap(uint32_t x) { return _byteswap_ulong(x); }
+    inline static int16_t byteSwap(int32_t x) { return (int16_t)_byteswap_ulong(x); }
+
+    // PCM data conversion to float
+    template <typename Type>
+    static void normalizeType(const QAudioFormat& format, const char* data, size_t dataSize, QVector<float>& out);
+    template <typename Type>
+    static void normalizeAudio(const Type* in, float* out, size_t outSize, bool isLittleEndian);
+
+private:
+    QVector<float> m_hanningFunction;
+    QVector<float> m_spikeConvFunction;
+};
+
+#endif // AUDIOCONVERSIONUTILS_H
