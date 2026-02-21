1760afee3e35c7dc141d709db39d62282c7ad3b7
brianuuu <38166666+brianuuu@users.noreply.github.com>
2026-02-21 20:52:31 +0000

    Add template program

---
+++ Programs/Development/template.cpp
@@ -0,0 +1,27 @@
+#include "template.h"
+
+namespace Program::CATEGORY
+{
+
+NAME_NO_SPACE::NAME_NO_SPACE(QObject *parent)
+    : ProgramBase{parent}
+{
+    
+}
+
+void NAME_NO_SPACE::PopulateSettings(QBoxLayout *layout)
+{
+    
+}
+
+void NAME_NO_SPACE::Start()
+{
+    ProgramBase::Start();
+}
+
+void NAME_NO_SPACE::Stop()
+{
+    ProgramBase::Stop();
+}
+
+}
---
+++ Programs/Development/template.h
@@ -0,0 +1,44 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#include "../programbase.h"
+
+namespace Program::CATEGORY
+{
+class NAME_NO_SPACE : public ProgramBase
+{
+public:
+    explicit NAME_NO_SPACE(QObject *parent = nullptr);
+
+    static QString GetCategory() { return "CATEGORY"; }
+    static QString GetName() { return "NAME_SPACE"; }
+
+    // from ProgramBase
+    void PopulateSettings(QBoxLayout* layout) override;
+    QString GetInternalName() const override { return "CATEGORY-NAME_NO_SPACE"; }
+    QString GetDescription() const override {
+        return "DESCRIPTION";
+    }
+
+    bool RequireSerial() const override { return true; }
+    bool RequireVideo() const override { return false; }
+    bool RequireAudio() const override { return false; }
+
+    bool CanControlWhileRunning() const override { return true; }
+    bool CanEditWhileRunning() const override { return true; }
+
+    void Start() override;
+    void Stop() override;
+
+private slots:
+
+
+private:
+
+
+private:
+
+};
+}
+
+#endif // TEMPLATE_H
